ed3eb28b50e334d851f6f83f9e2124713984d983
Przemysław Kusiak <przemyslaw.r.kusiak@gmail.com>
2023-05-25 10:40:48 +0200

    nvim: bring back <Tab> for snippets

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -329,19 +329,15 @@ local function SetupLsp()
                 behavior = cmp.ConfirmBehavior.Replace,
                 select = true,
             },
-            --['<Tab>'] = cmp.mapping(function(fallback)
-            --    if luasnip.expand_or_jumpable() then
-            --        luasnip.expand_or_jump()
-            --    elseif cmp.visible() then
-            --        cmp.select_next_item()
-            --    else
-            --        fallback()
-            --    end
-            --end, { 'i', 's' }),
+            ['<Tab>'] = cmp.mapping(function(fallback)
+                if luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end, { 'i', 's' }),
             ['<S-Tab>'] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.select_prev_item()
-                elseif luasnip.jumpable(-1) then
+                if luasnip.jumpable(-1) then
                     luasnip.jump(-1)
                 else
                     fallback()
