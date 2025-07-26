2c789bbc353dd0a5229362e680eecda251d94b50
Przemysław Kusiak <mail@allvpv.org>
2025-07-26 17:56:38 +0200

    bootstrap: Do not download completions, do not change the default shell

--- bootstrap.sh
+++ bootstrap.sh
@@ -42,8 +42,6 @@ function main {
   cd "${HOME}"
   git clone "https://www.github.com/allvpv/dotfiles.git" ".dotfiles"
 
-  download_completions
-
   case $OSTYPE in
     darwin*)
       NUSHELL_CONFIG_DIR="$HOME/Library/Application Support/nushell"
@@ -85,12 +83,7 @@ function main {
   backup "${SYMLINK_DST}"
   link
 
-  change_shell
-
   echo "Enjoy!"
-
-  # Run shell
-  $(command -v bash)
 }
 
 function backup {
@@ -128,46 +121,6 @@ function ask_yn {
 function link {
   local SYMLINK_SRC_FULL="$(realpath "${SYMLINK_SRC}")"
   ln -s "${SYMLINK_SRC_FULL}" "${SYMLINK_DST}"
-}
-
-function download_completions {
-  printf "Downloading completions"
-
-  mkdir -p "${HOME}/.bash_completions"
-
-  printf " [1/3]"
-
-  # `.git` basic completion
-  curl -fsSL \
-    "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash" -o \
-    "${HOME}/.bash_completions/git.sh"
-
-  printf " [2/3]"
-
-  # `.git` prompt completion (for __git_ps1)
-  curl -fsSL \
-    "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh" -o \
-    "${HOME}/.bash_completions/git-prompt.sh"
-
-  printf " [3/3]"
-
-  # `pass` password store completion
-  curl -fsSL \
-    "https://raw.githubusercontent.com/zx2c4/password-store/master/src/completion/pass.bash-completion" -o \
-    "${HOME}/.bash_completions/pass.sh"
-
-  echo " Done!"
-  # ...more completions are loaded in .bashrc from system directories
-}
-
-function change_shell {
-  BASH_EXE="$(command -v bash)"
-
-  ask_yn "Change default shell to '${BASH_EXE}' (y/n)?"
-
-  if [[ $answer == "y" ]]; then
-    chsh -s "${BASH_EXE}"
-  fi
 }
 
 main "$@" || exit 4
